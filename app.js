3f7d75112446afef20077ba0cd966be0d8788d4f
phillip wong <deepelegant99@gmail.com>
2023-03-19 23:18:08 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,69 @@
+
+require('dotenv').config()
+var createError = require('http-errors');
+var express = require('express');
+const compression = require("compression");
+const helmet = require("helmet");
+
+// Set up rate limiter: maximum of twenty requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.URI);
+}
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(helmet());
+app.use(compression()); // Compress all routes
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
